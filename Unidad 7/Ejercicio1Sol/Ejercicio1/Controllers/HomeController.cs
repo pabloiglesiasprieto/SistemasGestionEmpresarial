fcb601d594edd40d3fdd12903a985a856bb7febd
pabloiglesiasprieto <pablo.iglesias@iesnervion.es>
2025-10-20 14:37:58 +0200

    si

---
+++ Unidad 7/Ejercicio1Sol/Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using System.Diagnostics;
+using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Ejercicio1.Models.Entities;
+
+namespace Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string saludos;
+        private DateTime hora = DateTime.Now;
+        Persona persona = new Persona("Juan", 30);
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                saludos = "Buenos días";
+            }
+            else if (DateTime.Now.Hour < 13)
+            {
+                saludos =  "Buenas tardes";
+            }
+            else
+            {
+                saludos = "Buenas noches";
+            }
+            ViewData["Saludos"] = saludos;
+            ViewBag.Hora = hora;
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unidad 7/Ejercicio1Sol/Ejercicio1/Models/Entities/Persona.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio1.Models.Entities
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+
+        public Persona(string nombre, int edad)
+        {
+
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+
+
+        }
+        public int Edad
+        {
+            get { return this.edad; }
+            set { this.edad = value; }
+        }
+    }
+}
