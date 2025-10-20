31aba0a5d53da8bd97a140901544e50bd934fc2e
pabloiglesiasprieto <pablo.iglesias@iesnervion.es>
2025-10-20 13:58:42 +0200

    si

---
+++ Unidad 5/01HolaMundo/01HolaMundo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01HolaMundo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
